bee67f0a5f0bb3dd583866c068e338490b689150
binsiyabinu <51994726+binsiyabinu@users.noreply.github.com>
2019-11-06 14:36:24 +0530

    first commit
    Smart watch contains three icons,one for message , one for music and one for timer.

---
+++ Smart watch/watch.js
@@ -0,0 +1,19 @@
+showEvents({
+  "events": [
+     {
+       "Sender": "A",
+       "message": "Welcome",
+       "time": "12.45pm"
+     },
+     {
+       "Sender": "B",
+       "Message": "Hai ",
+       "time": "1pm"
+     },
+     {
+       "Sender": "C",
+       "Message": "Mrng",
+       "time": "3pm"
+     }
+   ]
+});
